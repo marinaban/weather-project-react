07bd926f8da278a7a548ade655ebb301fdc771cd
marinaban <marina.banich@gmail.com>
2022-04-05 12:54:28 +0200

    added bootstrap, fonts and video

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Submit from "./Submit";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div container>
+      <div row>
+        <div col>
+          <div className="App">
+            <h1 className="title"> WEATHER FORECAST</h1>
+            <Submit />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Submit.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Submit() {
+  return (
+    <div>
+      <form className="form">
+        <input type="search" />
+        <input type="submit" value="Search" />
+      </form>
+    </div>
+  );
+}
